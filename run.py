786213322ed804cf9a35c180298cff0c19642736
Edward Shanahan <edwardshanahan357@gmail.com>
2023-04-19 18:09:44 +0000

    Create and save tasks

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import os 
 from taskmanager import app
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP"),
         port=os.environ.get("PORT"),
